14911eda514c8e5a57cd060363db1b4ba0011560
SuzuMiyaYuuGi <ugkongthong@gmail.com>
2025-02-07 12:10:12 +0700

    update

---
+++ lib/adidas_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdidasScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Screen'),
+      ),
+      body: Center(
+        child: Text('Search Screen Content'),
+      ),
+    );
+  }
+}
